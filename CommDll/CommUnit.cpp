ad30a8685fd44d06191740202e83ae8ab2c4c391
yamanobori_old <yamanobori_old@yahoo.co.jp>
2015-08-08 20:48:19 +0900

    名前の変更

--- CommDll/CommUnit.cpp
+++ CommDll/CommUnit.cpp
@@ -20,16 +20,7 @@ BOOL CommUnit::Open(int comNumber) {
 	TCHAR comName[BUFSIZ] = {0};
 	success = createCommNumber(comName, BUFSIZ, comNumber);
 	if (success) {
-		mComHandle = ::CreateFile(
-			comName, /* シリアルポートの文字列 */
-			GENERIC_READ | GENERIC_WRITE, /* アクセスモード */
-			0,                            /* 共有モード */
-			NULL,                         /* セキュリティ属性 */
-			OPEN_EXISTING,                /* 作成フラグ */
-			FILE_ATTRIBUTE_NORMAL,        /* 属性 */
-			NULL                          /* テンプレートのハンドル */
-			);
-		success = mComHandle != INVALID_HANDLE_VALUE;
+		success = createCommHandle(comName, &mComHandle);
 	}
 
 	DCB dcb = { 0 };
@@ -38,8 +29,8 @@ BOOL CommUnit::Open(int comNumber) {
 	}
 
 	TCHAR def[BUFSIZ] = {0};
-	if ((_stprintf_s(def, BUFSIZ, _T("baud=%d parity=%c data=%d stop=%d"), 115200, 'N', 8, 1)) == -1) {
-		success = FALSE;
+	if (success) {
+		success = createCommParam(def, BUFSIZ);
 	}
 
 	if (success) {
@@ -54,8 +45,6 @@ BOOL CommUnit::Open(int comNumber) {
 BOOL CommUnit::Close() {
 	return CloseHandle();
 }
-
-
 
 BOOL CommUnit::CloseHandle() {
 	if (mComHandle != INVALID_HANDLE_VALUE) {
